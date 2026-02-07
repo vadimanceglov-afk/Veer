d39a1a7a63721d454edafc5e5e6ff47918e21f24
Вадим Янчеглов <vadimncheglov@gmail.com>
2026-02-07 15:21:36 +0200

    Test change

--- auth_system/urls.py
+++ auth_system/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("logout/", views.user_logout, name="logout")
+    path("logout/", views.user_logout, name="logout"),
+    path("login/", views.user_login, name="login"),
+    path("register/", views.user_register, name="register")
 ]
--- auth_system/views.py
+++ auth_system/views.py
@@ -1,6 +1,38 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 def user_logout(request):
     logout(request)
     return redirect("main_page")
+
+def user_login(request):
+    if request.method == "GET":
+        form = AuthenticationForm
+
+    elif request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get("username")
+            password = form.cleaned_data.get("password")
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect("main_page")
+
+
+    return render(request, template_name="auth_sistem/login.html", context={"form": form})
+
+
+def user_register(request):    
+    if request.method == "GET":
+        form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            new_user = form.save()
+            login(request, new_user)
+            return redirect("main_page")
+
+    return render(request, template_name="auth_sistem/register.html", context={"form": form})
